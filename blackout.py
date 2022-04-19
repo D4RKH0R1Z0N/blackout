372cf03f1750c97620d68e36b97f346f114a857e
D4RKH0R1Z0N <drkhrznorginal@gmail.com>
2022-04-19 14:10:29 +0530

    Create blackout.py

---
+++ blackout.py
@@ -0,0 +1,41 @@
+# Code written by D4RKH0R1Z0N
+# https://github.com/D4RKH0R1Z0N/blackout
+import screen_brightness_control as sbc
+import time
+
+default = 100
+blackout = 0
+timeout = 5
+loop_var = 0
+
+def screen_blackout():
+    sbc.set_brightness(blackout)
+    time.sleep(0.01)
+    sbc.set_brightness(blackout)
+    time.sleep(0.01)
+    sbc.set_brightness(blackout)
+    time.sleep(0.01)
+    sbc.set_brightness(blackout)
+    time.sleep(0.01)
+    sbc.set_brightness(blackout)
+    time.sleep(0.01)
+    sbc.set_brightness(blackout)
+    time.sleep(0.01)
+    sbc.set_brightness(blackout)
+    time.sleep(0.01)
+    sbc.set_brightness(blackout)
+    time.sleep(0.01)
+    sbc.set_brightness(blackout)
+
+def blackout_screen():
+    screen_blackout()
+    screen_blackout()
+    screen_blackout()
+
+runtime = timeout + 1
+
+while (loop_var < runtime):   
+    blackout_screen()
+    loop_var = loop_var + 1
+else:
+    sbc.set_brightness(default)
